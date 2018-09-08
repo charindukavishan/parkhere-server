f0a49f30b5078065e59857b886d6f8339f9b738f
Wageesh95 <imwageesha@gmail.com>
2018-09-08 14:37:18 +0700

    changed app.js

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-require('./config/config');
-require('./models/db');
-require('./config/passportConfig');
+// require('./config/config');
+// require('./models/db');
+// require('./config/passportConfig');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
+const PORT = process.env.PORT || 5000;
 
-const rtsIndex = require('./routes/index.router');
+// const rtsIndex = require('./routes/index.router');
 
 var app = express();
 
@@ -15,7 +16,10 @@ var app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.use(passport.initialize());
-app.use('/api', rtsIndex);
+app.get('/', (req, res) => {
+    res.send("hello");
+});
+// app.use('/api', rtsIndex);
 
 // error handler
 app.use((err, req, res, next) => {
@@ -28,4 +32,4 @@ app.use((err, req, res, next) => {
 });
 
 // start server
-app.listen(process.env.PORT, () => console.log(`Server started at port : ${process.env.PORT}`));
+app.listen(PORT, () => console.log(`Server started at port : ${PORT}`));
