2259a5ecd5cf5ec00f43efc25154c4dd9ce58f45
charindu <charindukavishan@gmail.com>
2019-01-23 10:38:57 +0530

    commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
-const port=process.env.PORT || 3000
+const PORT = process.env.PORT || 5000;
 
 const rtsIndex = require('./routes/index.router');
 
